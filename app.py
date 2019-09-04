37b390765f333223d402e0c72c5baa3d6bb5d7f1
andretheronsa <andretheronsa@gmail.com>
2019-09-04 20:21:23 +0100

    add image

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Simone!'
+    return 'Hello Simone! Go to /index :)'
+
+@app.route('/index', methods=['GET', 'POST'])
+def index(): 
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
